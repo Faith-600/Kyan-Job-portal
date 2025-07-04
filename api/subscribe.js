94b74f7948a7b42c9929b9b08c17b9464ecf1f70
Ezekiel Faith <ezekielfaith2001@gmail.com>
2025-07-04 19:30:12 +0100

    made some changes

--- api/subscribe.js
+++ api/subscribe.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_LIST_ID, {
-      email_address: email,
+    email_address: email,
       status: 'subscribed',
     });
 
