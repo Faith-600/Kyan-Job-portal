f33b0bac70ccfa8b5c9b82bb155abf70c93b2ec3
Ezekiel Faith <ezekielfaith2001@gmail.com>
2025-07-15 21:13:00 +0100

    made a change

--- api/subscribe.js
+++ api/subscribe.js
@@ -16,7 +16,6 @@ mailchimp.setConfig({
 });
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
